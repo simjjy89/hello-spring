38d6a4440c1d0226d085c9d3d21a5d1dda74c4a2
jaejin <simjjy89@gmail.com>
2021-09-09 15:43:05 +0900

    공부

---
+++ src/main/java/hello/hellospring/controller/HomeController.java
@@ -0,0 +1,11 @@
+package hello.hellospring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+}
